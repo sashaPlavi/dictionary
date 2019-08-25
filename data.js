cc92a6ad0248fe798c31b2011dee68b0a759172b
SashaPlavi <sasha.bingulac@gmail.com>
2019-08-25 12:57:15 +0200

    have response

--- data.js
+++ data.js
@@ -1,5 +1,15 @@
 function fechDef(word) {
   console.log(word);
+  let query = word.toLowerCase();
+  let key = "63e83d1c-a14e-412a-be96-1b811cba434e";
+  let endpoint = "entries";
+  let url = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${query}?key=${key} `;
+  console.log(url);
+  fetch(url)
+    .then(response => response.json())
+    .then(res => {
+      console.log(res);
+    });
 }
 
 export { fechDef };
